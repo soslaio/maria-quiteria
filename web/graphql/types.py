b5d49648337eeeab054cfe9bc8742c1e36d42d75
Rogério Pereira <rogerio.pereira@luizalabs.com>
2021-03-05 08:39:56 -0300

    Adição de mais modelos

--- web/graphql/types.py
+++ web/graphql/types.py
@@ -1,8 +1,33 @@
+import graphene
+from django.contrib.postgres.search import SearchVectorField
 from graphene_django import DjangoObjectType
-from web.datasets.models import Gazette
+from graphene_django.converter import convert_django_field
+from web.datasets.models import File, Gazette, GazetteEvent
+
+
+@convert_django_field.register(SearchVectorField)
+def convert_json_field_to_string(field, registry=None):
+    return graphene.String()
+
+
+class FileType(DjangoObjectType):
+    class Meta:
+        model = File
+        fields = ("id", "url", "content")
+
+
+class GazetteEventType(DjangoObjectType):
+    class Meta:
+        model = GazetteEvent
+        fields = "__all__"
 
 
 class GazetteType(DjangoObjectType):
+    files = graphene.List(FileType)
+
+    def resolve_files(self, info, **kwargs):
+        return File.objects.filter(object_id=self.id)
+
     class Meta:
         model = Gazette
         fields = "__all__"
